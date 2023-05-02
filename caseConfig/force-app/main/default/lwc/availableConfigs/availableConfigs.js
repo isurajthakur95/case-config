af7259824fce31a4464c2389972f2bf1fa8d11b2
Suraj Thakur <isurajthakur95@gmail.com>
2023-05-02 08:57:01 +0530

    Documenting

--- caseConfig/force-app/main/default/lwc/availableConfigs/availableConfigs.js
+++ caseConfig/force-app/main/default/lwc/availableConfigs/availableConfigs.js
@@ -1,61 +1,98 @@
-import { LightningElement , wire , track , api} from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import getConfigData from '@salesforce/apex/CaseConfigController.getConfigData';
 import addConfigData from '@salesforce/apex/CaseConfigController.addConfigData';
 import getCaseStatusValue from '@salesforce/apex/CaseConfigController.getCaseStatusValue';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { publish, subscribe , MessageContext } from 'lightning/messageService';
+import { publish, subscribe, MessageContext } from 'lightning/messageService';
 import REFRESH_CASE from '@salesforce/messageChannel/Refresh_Case__c';
 
 const columns = [
-    { label: 'Label', fieldName: 'Label__c', sortable: true },
-    { label: 'Type', fieldName: 'Type__c', sortable: true },
-    { label: 'Amount', fieldName: 'Amount__c', sortable: true }
+    { label: 'Label', fieldName: 'Label__c' },
+    { label: 'Type', fieldName: 'Type__c' },
+    { label: 'Amount', fieldName: 'Amount__c' }
 ];
 
-const errorVariant = 'error';
-const errorTitle = 'ERROR';
-const successVariant = 'success';
-const successTitle = 'SUCCESS';
+/**
+ * Constants for toast message variants and titles
+ */
+const ERROR_VARIANT = 'error';
+const ERROR_TITLE = 'ERROR';
+const SUCCESS_VARIANT = 'success';
+const SUCCESS_TITLE = 'SUCCESS';
 
+/**
+ * AvailableConfigs component
+ *
+ * Displays a list of available configurations and allows the user to select and add them to the current case.
+ */
 export default class AvailableConfigs extends LightningElement {
+    /** The record ID of the current case */
     @api recordId;
+
+    /** The configuration data to display in the table */
     @track configData = [];
-    columns = columns;
+
+    /** The columns to display in the table */
+    @track columns = [
+        { label: 'Label', fieldName: 'Label__c', sortable: true },
+        { label: 'Type', fieldName: 'Type__c', sortable: true },
+        { label: 'Amount', fieldName: 'Amount__c', sortable: true }
+    ];
+
+    /** The field to sort the data by */
     @track sortBy;
+
+    /** The direction to sort the data */
     @track sortDir;
+
+    /** Whether the Add button should be disabled */
     @track disableButton = true;
+
+    /** Whether the current case is closed */
     caseNotClosed = true;
 
-    @wire(getConfigData , {recordId : '$recordId'})
-    getConfigData({data,error}){
+    /** The message context to subscribe and publish messages */
+    @wire(MessageContext)
+    messageContext;
+
+    /**
+     * Gets the configuration data for the current case and sets the `configData` property.
+     */
+    @wire(getConfigData, { recordId: '$recordId' })
+    getConfigData({ data, error }) {
         if (data) {
             this.configData = data;
             this.handleGetCaseStatusValue();
         } else if (error) {
-            this.handleToastMessage(errorTitle, error , errorVariant);
+            this.handleToastMessage('ERROR', error, 'error');
         }
     }
 
-    handleGetCaseStatusValue(){
-        getCaseStatusValue({recordId : this.recordId})
-        .then(result =>{
-            this.caseNotClosed = result;
-        })
-        .catch(error =>{
-            this.handleToastMessage(errorTitle, error , errorVariant);
-        })
+    /**
+     * Gets the case status value for the current case and sets the `caseNotClosed` property.
+     */
+    handleGetCaseStatusValue() {
+        getCaseStatusValue({ recordId: this.recordId })
+            .then(result => {
+                this.caseNotClosed = result;
+            })
+            .catch(error => {
+                this.handleToastMessage('ERROR', error, 'error');
+            });
     }
 
+    /**
+     * Subscribes to the `Refresh_Case__c` message channel.
+     */
     connectedCallback() {
         this.subscribeToMessageChannel();
     }
 
-    doSort(event) {
-        this.sortBy = event.detail.fieldName;
-        this.sortDir = event.detail.sortDirection;
-        this.sortData(this.sortBy, this.sortDir);
-    }
-
+    /**
+     * Sort the data.
+     * @param {string} fieldname - The name of the field to sort by.
+     * @param {string} direction - The sorting direction.
+     */
     sortData(fieldname, direction) {
         let parseDataCloned = JSON.parse(JSON.stringify(this.configData));
         // Return the value stored in the field
@@ -73,7 +110,21 @@ export default class AvailableConfigs extends LightningElement {
         });
         this.configData = parseDataCloned;
     }
-
+    
+    /**
+     * Handle the sort event.
+     * @param {object} event - The event object.
+     */
+    doSort(event) {
+        this.sortBy = event.detail.fieldName;
+        this.sortDir = event.detail.sortDirection;
+        this.sortData(this.sortBy, this.sortDir);
+    }
+    
+    /**
+     * Handle the row selection action.
+     * @param {object} event - The event object.
+     */
     rowSelectionAction(event){
         const selectedRows = event.detail.selectedRows;
         if(selectedRows.length > 0 && this.caseNotClosed){
@@ -82,36 +133,56 @@ export default class AvailableConfigs extends LightningElement {
             this.disableButton = true;
         }
     }
-
+    
+    /**
+     * Handle the add button click.
+     * @param {object} event - The event object.
+     */
     handleClickAdd(event){
         var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
         this.handleAddConfigData(selectedRecords);
     }
 
+    /**
+     * Invokes the Apex method to insert selected records into Case Configurations and updates the component UI and the parent component
+     * @param {Array} selectedRecords - The selected records to be added to Case Configurations
+     */
     handleAddConfigData(selectedRecords){
         addConfigData({recordId : this.recordId, newConfigRecs : selectedRecords})
         .then(result =>{
             if(result){
-                this.handleToastMessage(successTitle, 'Records Inserted Successfully' , successVariant);
+                this.handleToastMessage(SUCCESS_TITLE, 'Records Inserted Successfully' , SUCCESS_VARIANT);
                 let clonedConfigData = this.configData;
                 this.configData = this.removeElements(clonedConfigData, selectedRecords);
                 this.refreshCaseRecord();
             }
         })
         .catch(error =>{
-            this.handleToastMessage(errorTitle, error , errorVariant);
+            this.handleToastMessage(ERROR_TITLE, error , ERROR_VARIANT);
         })
     }
 
+    /**
+     * Removes the elements from the configData array based on the currentSelectedRecords array
+     * @param {Array} currentConfigRecs - The current configuration records array
+     * @param {Array} currentSelectedRecords - The selected records array to be removed from the configData array
+     * @returns {Array} - The updated configData array with the selected records removed
+     */
     removeElements(currentConfigRecs, currentSelectedRecords){
         return currentConfigRecs.filter(currentConfigRec => {
             return !currentSelectedRecords.some(currentSelectedRec => currentConfigRec.Label__c === currentSelectedRec.Label__c);
         });
     }
 
+    /**
+     * Wire service to handle message context subscription
+     */
     @wire(MessageContext)
     messageContext;
 
+    /**
+     * Publishes the refresh event to the parent component on successful record insert
+     */
     refreshCaseRecord(){
         const caseData = {
             recordId: this.recordId,
@@ -120,6 +191,12 @@ export default class AvailableConfigs extends LightningElement {
         publish(this.messageContext, REFRESH_CASE, caseData);
     }
 
+    /**
+     * Displays the toast message with the specified title, message and variant
+     * @param {String} title - The title of the toast message
+     * @param {String} message - The message to be displayed in the toast
+     * @param {String} variant - The variant of the toast message
+     */
     handleToastMessage(title, message, variant){
         const evt = new ShowToastEvent({
             title: title,
@@ -129,6 +206,9 @@ export default class AvailableConfigs extends LightningElement {
         this.dispatchEvent(evt);
     }
 
+    /**
+     * Subscribes to the message channel to listen to refresh events
+     */
     subscribeToMessageChannel() {
         this.subscription = subscribe(
           this.messageContext,
@@ -137,8 +217,14 @@ export default class AvailableConfigs extends LightningElement {
         );
     }
 
+    /**
+     * Handles the refresh event and disables the Add button if the Case is Closed
+     * @param {Object} caseEventData - The data object of the refresh event
+     */
     handleRefreshCaseEvent(caseEventData){
-        if(caseEventData.recordId === this.recordId && caseEventData.action === 'disable')
+        if(caseEventData.recordId === this.recordId && caseEventData.action === 'disable'){
+            this.disableButton = true;
             this.caseNotClosed = false;
+        }
     }
 }
--- caseConfig/force-app/main/default/lwc/caseConfigs/caseConfigs.js
+++ caseConfig/force-app/main/default/lwc/caseConfigs/caseConfigs.js
@@ -1,4 +1,4 @@
-import { LightningElement , api , track , wire } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import getCaseConfigData from '@salesforce/apex/CaseConfigController.getCaseConfigData';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { publish, subscribe, MessageContext } from 'lightning/messageService';
@@ -11,45 +11,72 @@ const columns = [
     { label: 'Amount', fieldName: 'Amount__c' }
 ];
 
-const errorVariant = 'error';
-const errorTitle = 'ERROR';
-const successVariant = 'success';
-const successTitle = 'SUCCESS';
+// Define constants for toast messages
+const TOAST_ERROR_VARIANT = 'error';
+const TOAST_ERROR_TITLE = 'ERROR';
+const TOAST_SUCCESS_VARIANT = 'success';
+const TOAST_SUCCESS_TITLE = 'SUCCESS';
 
+/**
+ * A component to display and edit case configuration data.
+ * @extends LightningElement
+ */
 export default class CaseConfigs extends LightningElement {
+    /** Public property ID of the case record. */
     @api recordId;
+
+    /** The columns to display in the case configuration table. */
     columns = columns;
+
+    /** The case configuration data to display in the table. */
     @track caseConfigData = [];
+
+    /** Indicates whether the "Send to Finance" button should be disabled. */
     @track disableButton = true;
 
-    connectedCallback(){
+    /**
+     * Called when the component is connected to the DOM.
+     * Retrieves the case configuration data and subscribes to the REFRESH_CASE message channel.
+     */
+    connectedCallback() {
         this.handleGetCaseConfigData(this.recordId);
         this.subscribeToMessageChannel();
     }
 
-    handleGetCaseConfigData(recordId){
-        getCaseConfigData({recordId : recordId})
-        .then(result =>{
-            if(result){
-                this.caseConfigData = result;
-                if(result.length > 0 && result[0].Case__r.Status !== 'Closed'){
-                    this.disableButton = false;
-                }else{
-                    this.disableButton = true;
+    /**
+     * Retrieves the case configuration data for the given case record ID.
+     * @param {string} recordId - The ID of the case record.
+     */
+    handleGetCaseConfigData(recordId) {
+        getCaseConfigData({ recordId: recordId })
+            .then(result => {
+                if (result) {
+                    this.caseConfigData = result;
+                    if (result.length > 0 && result[0].Case__r.Status !== 'Closed') {
+                        this.disableButton = false;
+                    } else {
+                        this.disableButton = true;
+                    }
                 }
-            }
-        })
-        .catch(error =>{
-            this.handleToastMessage(errorTitle, error , errorVariant);
-        })
+            })
+            .catch(error => {
+                this.handleToastMessage(TOAST_ERROR_TITLE, error, TOAST_ERROR_VARIANT);
+            });
     }
 
-    handleClickSend(event){
+    /**
+     * Called when the "Send to Finance" button is clicked.
+     * Sends the case configuration data to the finance system and disables the button.
+     * Also publishes a message to the REFRESH_CASE channel to disable the "Add Config" button on the case page.
+     * @param {Event} event - The click event.
+     */
+    handleClickSend(event) {
         this.handleActionOnConfigData();
         this.disableButton = true;
         this.setDisableAddButton();
     }
 
+    // Method to display toast messages
     handleToastMessage(title, message, variant){
         const evt = new ShowToastEvent({
             title: title,
@@ -61,30 +88,38 @@ export default class CaseConfigs extends LightningElement {
 
     @wire(MessageContext)
     messageContext;
-
+    
+    // Subscribe to the message channel
     subscribeToMessageChannel() {
-        this.subscription = subscribe(
-          this.messageContext,
-          REFRESH_CASE,
-          caseData => this.handleRefreshCaseEvent(caseData)
-        );
+        try {
+            this._subscription = subscribe(
+              this.messageContext,
+              REFRESH_CASE,
+              caseData => this.handleReloadCaseConfigData(caseData)
+            );
+        } catch (error) {
+            throw new Error('Error while subscribing to message channel: ' + error);
+        }
     }
 
-    handleRefreshCaseEvent(caseEventData){
+    // Handle reload case config data event
+    handleReloadCaseConfigData(caseEventData){
         if(caseEventData.recordId === this.recordId && caseEventData.action === 'reload')
-            this.handleGetCaseConfigData(caseData.recordId);
+            this.handleGetCaseConfigData(caseEventData.recordId);
     }
 
+    // Method to handle action on config data
     handleActionOnConfigData(){
         actionOnConfigData({recordId : this.recordId})
         .then(result =>{
-            this.handleToastMessage(successTitle, 'Records Submitted Successfully' , successVariant);
+            this.handleToastMessage(TOAST_SUCCESS_TITLE, 'Records Submitted Successfully' , TOAST_SUCCESS_VARIANT);
         })
         .catch(error =>{
-            this.handleToastMessage(errorTitle, error.body.message , errorVariant);
+            this.handleToastMessage(TOAST_ERROR_TITLE, error.body.message , TOAST_ERROR_VARIANT);
         })
     }
 
+    // Method to publish message to disable add button
     setDisableAddButton(){
         const caseData = {
             recordId: this.recordId,
